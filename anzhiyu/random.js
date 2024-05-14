f6ed2fedd71c981705ec1a24f0416ca31ecd0ec0
MADAO-LUV <3335075714@qq.com>
2024-05-14 15:39:08 +0800

    Site updated: 2024-05-14 15:39:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/b0e2bb20.html","posts/4055ee82.html","posts/9bcefa22.html","posts/842d5faf.html","posts/4a17b156.html","posts/0.html","posts/5e52f62.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/b0e2bb20.html","posts/4055ee82.html","posts/9bcefa22.html","posts/4a17b156.html","posts/2266ffb9.html","posts/842d5faf.html","posts/5e52f62.html","posts/0.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
